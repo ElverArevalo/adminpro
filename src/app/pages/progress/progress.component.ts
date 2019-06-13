7fd048dadf5b0a123f5a348d4ec9e95477e24317
elverArevalo <elver.arevalo@gmail.com>
2019-06-13 18:50:14 -0500

    recibir cambios

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -6,23 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ProgressComponent implements OnInit {
-  progreso: number = 50;
+  
   constructor() { }
 
   ngOnInit() {
   }
-  valores(valor: number) {
-      if( this.progreso >=100  && valor > 0) {
-        this.progreso = 100
-        return
-      }
-      if( this.progreso ==0  && valor < 0) {
-        this.progreso = 0
-        return
-      }
-    this.progreso = this.progreso + valor
-  
-    
-  }
+
 
 }
